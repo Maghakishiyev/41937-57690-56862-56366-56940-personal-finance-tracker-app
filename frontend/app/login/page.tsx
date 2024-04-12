4326bd0e731d82c41ca27ee339b9bc943298ee43
Abrorjon <abdualiev03@inbox.ru>
2024-04-12 21:17:02 +0200

    initialized signup and signin pages

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type Props = {}
+
+const LoginPage = (props: Props) => {
+  return (
+    <section className="flex align-middle justify-between bg-white rounded shadow-md max-w-8xl max-h-[80vh]  mx-auto mt-16 pt-[30px] pb-8">
+        
+        <div className='bg-gradient-to-tr from-[#0034D3] to-[#92C5FA]'>
+            <h1 className=" text-white text-[44px] font-bold font-jura">CashTrack</h1>
+            <div>
+                <div className='bg-white '></div>
+                <div></div>
+                <div></div>
+            </div>    
+        </div>
+        <div></div>
+    </section>
+  )
+}
+
+export default LoginPage
