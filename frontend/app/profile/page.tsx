536320944c2f541b020e6c3dd9dd489120f5180b
Movsum Aghakishiyev <95032908+Maghakishiyev@users.noreply.github.com>
2024-04-14 19:55:06 +0200

    feat: profile page (#14)

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 
 import { LoadingIcon } from '@/assets';
+import {
+    ExpensesTableContainer,
+    MonthlyReportContainer,
+    ProfileInfoContainer,
+} from '@/components/ProfileComponents';
 import { AccountState } from '@/store/UserStore';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -8,24 +13,25 @@ import { useSnapshot } from 'valtio';
 
 const ProfilePage = () => {
     const router = useRouter();
-    const { isUserLoading, isUserLoggedIn, user } = useSnapshot(AccountState);
+    const { isUserLoading, isUserLoggedIn } = useSnapshot(AccountState);
 
     useEffect(() => {
-        console.log('User is', user);
         if (!isUserLoading && !isUserLoggedIn) {
             router.replace('/login');
         }
     }, [isUserLoading, isUserLoggedIn]);
 
-    return (
-        <div>
-            {isUserLoading || !isUserLoggedIn ? (
-                <div className='w-full h-full min-h-screen flex items-center justify-center'>
-                    <LoadingIcon className='w-24 h-24 flex-shrink-0 animate-spin' />
-                </div>
-            ) : (
-                <div className='w-fit h-fit mx-auto bg-white '></div>
-            )}
+    return isUserLoading || !isUserLoggedIn ? (
+        <div className='w-full h-full min-h-screen flex items-center justify-center'>
+            <LoadingIcon className='w-24 h-24 flex-shrink-0 animate-spin' />
+        </div>
+    ) : (
+        <div className='w-fit h-full mx-auto bg-white flex gap-4 px-6 py-8 rounded-xl shadow-sm mt-16 min-w-[1200px]'>
+            <ProfileInfoContainer />
+            <div className='flex-grow w-full h-full min-h-full flex flex-col items-start gap-6'>
+                <MonthlyReportContainer />
+                <ExpensesTableContainer />
+            </div>
         </div>
     );
 };
