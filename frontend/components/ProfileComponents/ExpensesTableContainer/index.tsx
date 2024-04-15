2d5cd532b6c5151433406518241e4dc0b844cdce
Movsum Aghakishiyev <95032908+Maghakishiyev@users.noreply.github.com>
2024-04-15 06:53:51 +0200

    feat: add table and tabs for profile page (#15)

--- frontend/components/ProfileComponents/ExpensesTableContainer/index.tsx
+++ frontend/components/ProfileComponents/ExpensesTableContainer/index.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { columns, rows } from './constantsAndTypes';
+import { Tab, Tabs } from '@mui/material';
 
 export const ExpensesTableContainer: React.FC = () => {
+    const [value, setValue] = useState<number>(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
     return (
-        <div className='w-full h-full min-h-full flex-grow shadow-sm border-[#DCDCDC] border bg-white rounded py-4 '></div>
+        <div className='w-full flex-grow shadow-sm border-[#DCDCDC] border bg-white rounded py-4 flex flex-col items-start gap-8'>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                variant='scrollable'
+                scrollButtons='auto'
+                aria-label='scrollable auto tabs example'
+            >
+                <Tab label='All Transactions' />
+                <Tab label='Category A' />
+                <Tab label='Category B' />
+                <Tab label='Category C' />
+            </Tabs>
+            <div className='w-full flex flex-grow'>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    sx={{ border: 'none' }}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 7 },
+                        },
+                    }}
+                    pageSizeOptions={[7, 10]}
+                />
+            </div>
+        </div>
     );
 };
