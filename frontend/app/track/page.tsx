dbed8fa536ae39834f04ea84e6a74ca419cafe9b
Abror <106076418+Abdualiev01@users.noreply.github.com>
2024-05-24 23:20:54 +0200

    feat: implement Track Page

---
+++ frontend/app/track/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import TrackFields from '@/components/layout/TrackFields'
+import React from 'react'
+
+const TrackPage = () => {
+    return (
+        <section className='flex align-middle items-center gap-x-40 bg-white rounded shadow-md max-w-xl  mx-auto my-16'>
+            <TrackFields />
+        </section>
+    )
+}
+
+export default TrackPage
